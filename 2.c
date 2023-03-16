dde4c1aa1b47b28e4a29fa64f9c05effcb2f7cb3
DiveshGupta004 <122595975+DiveshGupta004@users.noreply.github.com>
2023-03-16 10:53:38 +0530

    added c file

---
+++ 2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+float calculate_area(float radius);
+float calculate_circumference(float radius);
+
+int main() {
+    float radius, area, circumference;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = calculate_area(radius);
+    circumference = calculate_circumference(radius);
+
+    printf("The area of the circle is: %.2f\n", area);
+    printf("The circumference of the circle is: %.2f\n", circumference);
+
+    return 0;
+}
+
+float calculate_area(float radius) {
+    return PI * radius * radius;
+}
+
+float calculate_circumference(float radius) {
+    return 2 * PI * radius;
+}
